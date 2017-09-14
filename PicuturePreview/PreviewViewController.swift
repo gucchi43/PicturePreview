947eb0b5aa1543a417b3e09d44a7488936e4dbc7
Hiroki Taniguchi <acmican43@gmail.com>
2017-09-15 04:08:59 +0900

    ベース作成  - ピンチイン、ピンチアウトでズーム  - ダブルタップでズーム  - シングルタップでヘッダー、フッダーを表示、非表示

---
+++ PicuturePreview/PreviewViewController.swift
@@ -0,0 +1,112 @@
+//
+//  PreviewViewController.swift
+//  PicuturePreview
+//
+//  Created by Hiroki Taniguchi on 2017/09/14.
+//  Copyright © 2017年 Hiroki Taniguchi. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var toolBar: UITabBar!
+    
+    var selectedImage : UIImage?
+    var allPreviewFlag = false
+    
+    @IBOutlet weak var previewScrollView: UIScrollView!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.hidesBottomBarWhenPushed = true
+        navigationController?.popViewController(animated: true)
+        
+        // スクロールビューの設定
+        self.previewScrollView.delegate = self
+        self.previewScrollView.minimumZoomScale = 1
+        self.previewScrollView.maximumZoomScale = 8
+        self.previewScrollView.isScrollEnabled = true
+        self.previewScrollView.showsHorizontalScrollIndicator = true
+        self.previewScrollView.showsVerticalScrollIndicator = true
+        
+        myInit()
+        
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action:#selector(self.doubleTap))
+        doubleTapGesture.numberOfTapsRequired = 2
+        self.previewImageView.addGestureRecognizer(doubleTapGesture)
+        let singleTapGesture = UITapGestureRecognizer(target: self, action:#selector(self.singleTap))
+        singleTapGesture.numberOfTapsRequired = 1
+        self.previewImageView.addGestureRecognizer(singleTapGesture)
+    }
+    
+    func myInit() {
+        // 画像を読み込んで配置する
+        if selectedImage != nil{
+            previewImageView?.image = selectedImage
+            previewImageView?.isUserInteractionEnabled = true
+            previewImageView?.contentMode = UIViewContentMode.scaleAspectFit
+        }else {
+            print("写真がない")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        //print("pinch")
+        return self.previewImageView
+    }
+    
+    func doubleTap(gesture: UITapGestureRecognizer) -> Void {
+        //print(self.myScrollView.zoomScale)
+        if (self.previewScrollView.zoomScale < self.previewScrollView.maximumZoomScale) {
+            let newScale = self.previewScrollView.zoomScale * 3
+            let zoomRect = self.zoomRectForScale(scale: newScale, center: gesture.location(in: gesture.view))
+            self.previewScrollView.zoom(to: zoomRect, animated: true)
+        } else {
+            self.previewScrollView.setZoomScale(1.0, animated: true)
+        }
+    }
+    
+    func zoomRectForScale(scale:CGFloat, center: CGPoint) -> CGRect{
+        let size = CGSize(
+            width: self.previewScrollView.frame.size.width / scale,
+            height: self.previewScrollView.frame.size.height / scale
+        )
+        return CGRect(
+            origin: CGPoint(
+                x: center.x - size.width / 2.0,
+                y: center.y - size.height / 2.0
+            ),
+            size: size
+        )
+    }
+    
+    func singleTap(gesture: UITapGestureRecognizer) -> Void {
+        if self.previewScrollView.zoomScale == 1.0{
+            print("ズーム無し状態")
+            chengeHederandFoter()
+        }else {
+            print("ズームあり状態")
+            chengeHederandFoter()
+        }
+    }
+    
+    func chengeHederandFoter(){
+        if allPreviewFlag == false {
+            toolBar.isHidden = true
+            navigationController?.navigationBar.isHidden = true
+            allPreviewFlag = true
+        }else {
+            toolBar.isHidden = false
+            navigationController?.navigationBar.isHidden = false
+            allPreviewFlag = false
+        }
+    }
+}
--- PicuturePreview/ViewController.swift
+++ PicuturePreview/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let photoStringArray = ["a","b","c","d","e","f","g","h"]
+    var selectedImage : UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoStringArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize:CGFloat = self.view.frame.size.width/2-2
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as UICollectionViewCell
+        let cellImageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let cellimage = UIImage(named:photoStringArray[indexPath.row])
+        cellImageView.image = cellimage
+        return cell
+    }
+    
+    // Cell が選択された場合
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // [indexPath.row] から画像名を探し、UImage を設定
+        selectedImage = UIImage(named:photoStringArray[indexPath.row])
+        if selectedImage != nil {
+            // SubViewController へ遷移するために Segue を呼び出す
+            performSegue(withIdentifier: "toPreview",sender: nil)
+        }
+        
+    }
+    
+    // Segue 準備
+    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        if (segue.identifier == "toPreview") {
+            let navigationViewController = segue.destination as! UINavigationController
+            let preVC = navigationViewController.viewControllers[0] as! PreviewViewController
+            // SubViewController のselectedImgに選択された画像を設定する
+            preVC.selectedImage = selectedImage
+        }
+    }
 }
 
